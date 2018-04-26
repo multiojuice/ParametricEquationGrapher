f1f10d1569f70cd507f22ac8d327f4a24a0c1f0b
Owen J Sullivan <multiojuice@gmail.com>
2018-04-25 21:06:04 -0400

    Got the graphing working for a simple equation, adding more soon.

--- parametric_graph.py
+++ parametric_graph.py
@@ -7,25 +7,20 @@ desc - TBD
 import turtle
 import math
 
-def get_initial_points():
+def get_initial_points(function):
     """
     desc - Gets the x and y coordinates for a given point
     return - the array of points as [[x,y],[x,y],[x,y]] 
         sorted in ascending order of x.
     """
-    start_x = -1
-    end_x = 1
-    dx = .1
+    start_theta = 0
+    end_theta =  math.pi
+    dx = .05
     points = []
-    for i in range(start_x,end_x+1,dx):
-        temp = []
-        temp[0] = math.sin(math.pi * 2 * i)
-        temp[1] = math.cos(math.pi * 2 * i)
-        points.append(temp)
-    # Still being worked on
-
-
-
+    while start_theta <= end_theta:
+        points.append([start_theta,function(start_theta)])
+        start_theta += dx
+    return points    
 
 
 def make_points_continous(points):
@@ -35,6 +30,23 @@ def make_points_continous(points):
     return - the array of points as [[x,y],[x,y],[x,y]] 
         sorted in a "continous order". 
     """
+    pass
+
+def draw_graph(points):
+    turtle.clear()
+    turtle.pd()
+    for point in points:
+        turtle.goto(0,0)
+        turtle.seth(0)
+        turtle.left(int(point[0] * 180 / math.pi))
+        turtle.forward(int(point[1] * 100))
+
+def draw_graph_cont(points):
+    turtle.clear()
+    turtle.pu()
+
+turtle.speed(9)
+draw_graph(get_initial_points(math.sin))
 
 
 
